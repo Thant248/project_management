843ca71d4c64716f2fc63f003981502f33360783
ASUS <thantsinphyoe18920@gmail.com>
2024-11-27 23:12:13 +0630

    add qr code

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Mail\InvitationMail;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class InvitationController extends Controller
 {
@@ -15,17 +16,17 @@ class InvitationController extends Controller
     }
 
     public function store(StoreUserRequest $request){
-        
+
         $data = $request->validated();
 
         $data['email_verified_at'] = time();
         $password = 'password';
         $data['password'] = bcrypt($password);
-
-        Mail::to($data['email'])->send(new InvitationMail($data['name'], $password, $data['email']));
+        $qrCode = QrCode::format('svg')->size(300)->generate("{$data['name']}");
+        Mail::to($data['email'])->send(new InvitationMail($data['name'], $password, $data['email'], $qrCode));
         User::create($data);
 
-        return to_route('invite.index')->with('success', 'User has been successfully invited');
-        
+        return to_route('user.index')->with('success', 'User has been successfully invited');
+
     }
 }
--- app/Mail/InvitationMail.php
+++ app/Mail/InvitationMail.php
@@ -3,34 +3,37 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\HtmlString;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class InvitationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
     public string $name;
-    public string $passowrd;
+    public string $password;
 
     public string $email;
 
-    public function __construct(string $name, string $password, string $email)
+    public HtmlString $qrCode;
+
+    public function __construct(string $name, string $password, string $email, HtmlString $qrCode)
     {
         $this->name = $name;
-        $this->passowrd = $password;
+        $this->password = $password;
         $this->email = $email;
+        $this->qrCode = $qrCode;
     }
 
     public function build(): self
     {
         return $this->view('emails.invitation', [
             'name' => $this->name,
-            'password' => $this->passowrd,
+            'password' => $this->password,
             'email' => $this->email,
+            'qrCode' => $this->qrCode,
         ])->subject('【ドクターズレシピ】 会員登録いただきありがとうございます');
     }
 
