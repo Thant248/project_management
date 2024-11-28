5df4d7839491a500e87bbac5431a0b03b0b277b8
Thant248 <thantsinphyoe18920@gmail.com>
2024-11-28 11:18:11 +0900

    invitation

--- app/Mail/InvitationMail.php
+++ app/Mail/InvitationMail.php
@@ -6,7 +6,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\HtmlString;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class InvitationMail extends Mailable
 {
